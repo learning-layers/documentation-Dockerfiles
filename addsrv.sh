fbf8c3947d4753d36696170198299c0fa8a7d07d
jrwthll <manolov@dbis.rwth-aachen.de>
2015-10-22 09:08:52 +0200

    initial commit

---
+++ addsrv.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# This small script serves to make a LayersBox Adapter aware of a new backend service.
+# Will become obsolete in the future as there should be only one such script included in the default LayersBox.
+
+
